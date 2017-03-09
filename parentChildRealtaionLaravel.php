10409623dad5ca53e9da3e8adb74c49751652ec0
ahmedfekry5 <ahmed.fekry@ivas.com.eg>
2017-03-09 12:38:54 +0200

    fekry adding the partent child migragration code

--- parentChildRealtaionLaravel.php
+++ parentChildRealtaionLaravel.php
@@ -4,9 +4,11 @@
 		Email :- ahmedfikry78@gmail.com
 		GitHub Account :- github.com/ahmedfekry
 		
-		this two functions is used to create parent child relationship in laravel 
+		this functions is used to create parent child relationship in laravel 
 		
 	*/
+
+		// add this to User Model
 	public function parent()
     {
         return $this->belongsTo('App\Model', 'parent_id');
@@ -17,6 +19,20 @@
         return $this->hasMany('App\Model', 'parent_id');
     }
 
+    // add this to a migration file 
+     /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->integer('parent_id')->unsigned()->nullable(); 
+            $table->foreign('parent_id')->references('id')->on('users')->onDelete('cascade');
+            //
+        });
+    }
 
 
  ?>
